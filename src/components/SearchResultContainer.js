edc257f6611ffa242faa695a0121bd1a8e111d27
JC <sanjothebay@gmail.com>
2021-02-28 23:27:11 -0600

    asking for help as the other askBCS guy just dont want to help bad qulaity

--- src/components/SearchResultContainer.js
+++ src/components/SearchResultContainer.js
@@ -17,17 +17,17 @@ class SearchResultContainer extends React.Component {
     //make the call to the API
     //thiss is going to execute only after the first render
     API.getUsers().then((res) => {
-      console.log("RES", res.data.results);
       this.setState({ users: res.data.results });
       this.setState({ allUsers: res.data.results });
     });
-
   }
 
   handlesSortChange = () => {
-    const copyOFusersBeingSorted = [...this.state.users]
-    copyOFusersBeingSorted.sort((a,b) => (a.email > b.email) ? 1 : ((b.email > a.email) ? -1 : 0))
-   
+    const copyOFusersBeingSorted = [...this.state.users];
+    copyOFusersBeingSorted.sort((a, b) =>
+      a.email > b.email ? 1 : b.email > a.email ? -1 : 0
+    );
+
     this.setState({
       users: copyOFusersBeingSorted,
     });
@@ -37,12 +37,16 @@ class SearchResultContainer extends React.Component {
     const name = event.target.name;
     const value = event.target.value;
     const result = this.state.users.filter((employee) => {
-      console.log(employee.name);
-      let employeeStr = Object.values(employee.name.first && employee.name.last && employee.email && employee.phone && employee.dob.date).join("");
+      let employeeStr = Object.values(
+        employee.name.first &&
+          employee.name.last &&
+          employee.email &&
+          employee.phone &&
+          employee.dob.date
+      ).join("");
       return employeeStr.toLowerCase().indexOf(value.toLowerCase()) !== -1;
     });
 
-    console.log( result );
     this.setState({
       [name]: value,
     });
@@ -52,7 +56,6 @@ class SearchResultContainer extends React.Component {
   };
 
   render() {
-    console.log("SSTATE", this.state.users);
     return (
       <div>
         <div>
@@ -64,7 +67,7 @@ class SearchResultContainer extends React.Component {
         <Table striped bordered hover>
           <thead>
             <tr>
-              <th >Image</th>
+              <th>Image</th>
               <th onClick={this.handlesSortChange}>Name</th>
               <th onClick={this.handlesSortChange}>Phone</th>
               <th onClick={this.handlesSortChange}>Email</th>
@@ -73,7 +76,6 @@ class SearchResultContainer extends React.Component {
           </thead>
           <tbody>
             {this.state.users.map((user, i) => {
-              //console.log(user.name.first)
               return (
                 <UserResultsList
                   name={user.name.first}
